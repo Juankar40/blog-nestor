ae6f25ac6a993c841e21c7f8e59df4e3f9305010
Gabriel-Boiza <gabriel.boiza@inslapineda.cat>
2025-05-11 19:33:46 +0200

    cambio nombres

--- js/home.js
+++ js/home.js
@@ -1,11 +1,3 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    getBooks()
 
-    async function getBooks() {
-        const response = await fetch("https://v2.api.noroff.dev/blog/posts/Nestor")
-        const data = await response.json()
-        const books = data.data
-        console.log(books);
-        
-    }
 })
